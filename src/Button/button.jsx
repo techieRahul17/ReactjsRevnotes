52b6dde6c1789ae8497d9582e14174fa6da35aa8
Rahul V S <117752342+techieRahul17@users.noreply.github.com>
2025-07-19 00:41:16 +0530

    Day 4 props done with css

---
+++ src/Button/button.jsx
@@ -0,0 +1,6 @@
+import styles from './Button.module.css'
+export default function Button() {
+    return (
+        <button className={styles.button}>Hi Rahul</button>
+    )
+}
--- src/ColorPicker.jsx
+++ src/ColorPicker.jsx
@@ -1,3 +1,14 @@
+import React,{useState} from 'react';
+
 export default function ColorPicker() {
-    return (<></>);
+
+    const [color, setColor] = useState('red');
+    return (<div className="color-picker">
+        <h1>Color Picker</h1>
+        <div className="color-display" style={{backgroundColor:color}}>
+            <p>Color Picker11</p>
+        </div>
+        <label>Select a color: </label>
+        <input onChange={(e) => setColor(e.target.value)}/>
+    </div>);
 }
